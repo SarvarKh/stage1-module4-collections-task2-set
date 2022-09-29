27b37a5922730071dbdd6ead17f0b2518c5cc3f1
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-09-29 10:39:12 +0500

    Second way of solving

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,19 +1,14 @@
 package com.epam.mjc.collections.set;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
-        Set<Integer> set = new TreeSet<Integer>();
+        NavigableSet<Integer> set = new TreeSet<Integer>();
         for (Integer item : sourceList) {
-            Integer squareOfItem = (int)Math.pow(item, 2);
-            if (lowerBound <= squareOfItem && squareOfItem <= upperBound) {
-                set.add(squareOfItem);
-            }
+            set.add((int)Math.pow(item, 2));
         }
-        return set;
+
+        return set.subSet(lowerBound, true, upperBound, true);
     }
 }
